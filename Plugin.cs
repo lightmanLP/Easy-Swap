3d98d913091c8aab25de0abbefc087fabdf51c78
the_cat <101831734+the-cat-d@users.noreply.github.com>
2025-03-22 19:54:14 +0300

    Organized Plugin Config into its own file

--- Plugin.cs
+++ Plugin.cs
@@ -1,29 +1,26 @@
 ﻿using BepInEx;
 using BepInEx.Logging;
-using PluginConfig.API;
-using PluginConfig.API.Fields;
+using GameConsole.Commands;
+using HarmonyLib;
 using System.IO;
 using System.Reflection;
 using UnityEngine;
+using UnityEngine.Device;
+using UnityEngine.SceneManagement;
 
 namespace Easy_Swap
 {
 
 
-    [BepInPlugin(MyGUID, PluginName, VersionString)]
+    [BepInPlugin(GUID, PluginName, VersionString)]
     public class Plugin : BaseUnityPlugin
     {
         // Mod Details
 
-        private const string MyGUID = "com.the_cat.Easy_Swap";
-        private const string PluginName = "Easy_Swap";
-        private const string VersionString = "1.2.2";
+        public const string GUID = "com.the_cat.Easy_Swap";
+        public const string PluginName = "Easy_Swap";
+        public const string VersionString = "1.3.0";
 
-
-
-        // Plugin Config
-
-        private PluginConfigurator config;
 
         // Enums
 
@@ -52,189 +49,13 @@ namespace Easy_Swap
         }
 
 
-        /// Keybinds \\\
 
-        #region
-
-        // Revovler
-
-        KeyCodeField RevovlerBlue;
-
-        KeyCodeField RevovlerGreen;
-
-        KeyCodeField RevovlerRed;
-
-
-        // Shotgun
-
-        KeyCodeField ShotgunBlue;
-
-        KeyCodeField ShotgunGreen;
-
-        KeyCodeField ShotgunRed;
-
-
-        // Nailgun
-
-        KeyCodeField NailgunBlue;
-
-        KeyCodeField NailgunGreen;
-
-        KeyCodeField NailgunRed;
-
-
-        // Railcannon
-
-        KeyCodeField RailcannonBlue;
-
-        KeyCodeField RailcannonGreen;
-
-        KeyCodeField RailcannonRed;
-
-
-        // Rocket Launcher
-
-        KeyCodeField RocketBlue;
-
-        KeyCodeField RocketGreen;
-
-        KeyCodeField RocketRed;
-
-        #endregion
-
-
-
-        public static string DefaultParentFolder = $"{Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)}";
+        public static string AssemblyFolder = $"{Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)}";
 
         public static ManualLogSource Log = new ManualLogSource(PluginName);
 
 
-
-        /// <summary>
-        /// Initialize the mod and PluginConfigurator.
-        /// 
-        /// 
-        /// Assign the Keycode variables 
-        /// </summary>
-
-
-        private void Awake()
-        {
-
-
-
-            Logger.LogInfo($"{PluginName},V{VersionString} is loading...");
-
-            //// Plugin Config Init \\\\
-
-            config = PluginConfigurator.Create("Easy Swap", MyGUID);
-
-
-
-            config.SetIconWithURL($"{Path.Combine(DefaultParentFolder, "icon.png")}");
-
-            /// Keybinds \\\
-
-            #region
-
-            float ColorValue = 0.7f; // Adjusts the brightness of the background color for the keybind field
-
-
-
-            // Revovler \\
-
-            ConfigPanel RevovlerContainer = new ConfigPanel(config.rootPanel, "Revovler", "revlovPanelES");
-
-
-            RevovlerBlue = new KeyCodeField(RevovlerContainer, "Revovler (Piercer)", "RevlovES1", KeyCode.None);
-            RevovlerBlue.fieldColor = new Color(0f, 0f, ColorValue);
-
-            RevovlerGreen = new KeyCodeField(RevovlerContainer, "Revovler (Marksman)", "RevlovES2", KeyCode.None);
-            RevovlerGreen.fieldColor = new Color(0f, ColorValue, 0f);
-
-            RevovlerRed = new KeyCodeField(RevovlerContainer, "Revovler (Sharpshooter)", "RevlovES3", KeyCode.None);
-            RevovlerRed.fieldColor = new Color(ColorValue, 0f, 0f);
-
-
-            // Shotgun \\
-
-            ConfigPanel ShotgunContainer = new ConfigPanel(config.rootPanel, "Shotgun", "ShotPanelES");
-
-
-            ShotgunBlue = new KeyCodeField(ShotgunContainer, "Shotgun (Core Eject)", "ShotES1", KeyCode.None);
-            ShotgunBlue.fieldColor = new Color(0f, 0f, ColorValue);
-
-            ShotgunGreen = new KeyCodeField(ShotgunContainer, "Shotgun (Pump)", "ShotES2", KeyCode.None);
-            ShotgunGreen.fieldColor = new Color(0f, ColorValue, 0f);
-
-            ShotgunRed = new KeyCodeField(ShotgunContainer, "Shotgun (Sawed On)", "ShotES3", KeyCode.None);
-            ShotgunRed.fieldColor = new Color(ColorValue, 0f, 0f);
-
-
-            // Nailgun \\
-
-            ConfigPanel NailgunContainer = new ConfigPanel(config.rootPanel, "Nailgun", "NailPanelES");
-
-
-            NailgunBlue = new KeyCodeField(NailgunContainer, "Nailgun (Attractor)", "NailES1", KeyCode.None);
-            NailgunBlue.fieldColor = new Color(0f, 0f, ColorValue);
-
-            NailgunGreen = new KeyCodeField(NailgunContainer, "Nailgun (Overheat)", "NailES2", KeyCode.None);
-            NailgunGreen.fieldColor = new Color(0f, ColorValue, 0f);
-
-            NailgunRed = new KeyCodeField(NailgunContainer, "Nailgun (Jumpstart)", "NailES3", KeyCode.None);
-            NailgunRed.fieldColor = new Color(ColorValue, 0f, 0f);
-
-
-            // Railcannon \\
-
-            ConfigPanel RailcannonContainer = new ConfigPanel(config.rootPanel, "Railcannon", "railPanelES");
-
-
-            RailcannonBlue = new KeyCodeField(RailcannonContainer, "Railcannon (Electric)", "RailES1", KeyCode.None);
-            RailcannonBlue.fieldColor = new Color(0f, 0f, ColorValue);
-
-            RailcannonGreen = new KeyCodeField(RailcannonContainer, "Railcannon (Screwdriver)", "RailES2", KeyCode.None);
-            RailcannonGreen.fieldColor = new Color(0f, ColorValue, 0f);
-
-            RailcannonRed = new KeyCodeField(RailcannonContainer, "Railcannon (Malicious)", "RailES3", KeyCode.None);
-            RailcannonRed.fieldColor = new Color(ColorValue, 0f, 0f);
-
-
-            // Rocket Launcher \\
-
-            ConfigPanel RocketContainer = new ConfigPanel(config.rootPanel, "Rocket Launcher", "railPanelES");
-
-
-            RocketBlue = new KeyCodeField(RocketContainer, "Rocket (Freeze Frame)", "RocketES1", KeyCode.None);
-            RocketBlue.fieldColor = new Color(0f, 0f, ColorValue);
-
-            RocketGreen = new KeyCodeField(RocketContainer, "Rocket (S.R.S)", "RocketlES2", KeyCode.None);
-            RocketGreen.fieldColor = new Color(0f, ColorValue, 0f);
-
-            RocketRed = new KeyCodeField(RocketContainer, "Rocket (Firestarter)", "RocketES3", KeyCode.None);
-            RocketRed.fieldColor = new Color(ColorValue, 0f, 0f);
-
-            #endregion
-
-
-
-            Logger.LogInfo($"{PluginName},V{VersionString} is loaded.");
-
-            // Sets up our static Log, so it can be used elsewhere in code.
-            // .e.g.
-            // Easy_SwapPlugin.Log.LogDebug("Debug Message to BepInEx log file");
-            Log = Logger;
-        }
-
-
-
-        /// <summary>
-        /// Equip the specified weapon (plus variant)
-        /// </summary>
-        /// <param name="WeaponEnum"></param>
-
-
+        // Equip the specified weapon (plus variant)
         private static void EquipWeapon(Guns WeaponEnum)
         {
 
@@ -473,186 +294,249 @@ namespace Easy_Swap
         }
 
 
+        public static Color[] GetVariantColors()
+        {
 
-        /// <summary>
-        /// Runs Every Frame. 
-        /// 
-        /// Here we check if the player presses any of the keybinds assigned.
-        /// </summary>
+            
+
+            Color[] variantColors = MonoSingleton<ColorBlindSettings>.Instance.variationColors;
+
+            Color blue = new Color(variantColors[0].r, variantColors[0].g, variantColors[0].b);
+            Color green = new Color(variantColors[1].r, variantColors[1].g, variantColors[1].b);
+            Color red = new Color(variantColors[2].r, variantColors[2].g, variantColors[2].b);
 
 
+            Color[] colors = {blue,green,red };
+
+            return colors;
+
+
+        }
+
+        // Initialize the mod, PluginConfigurator and assign the Keycode variables 
+
+        private void Awake()
+        {
+
+
+
+            Logger.LogInfo($"{PluginName},V{VersionString} is loading...");
+
+            
+
+
+
+
+
+
+
+            Logger.LogInfo($"{PluginName},V{VersionString} is loaded.");
+
+            // Sets up our static Log, so it can be used elsewhere in code.
+            // .e.g.
+            // Easy_SwapPlugin.Log.LogDebug("Debug Message to BepInEx log file");
+            Log = Logger;
+        }
+
+
+
+       
+
+        static bool configCreated = false;
+
+        // Runs Every Frame. Here we check if the player presses any of the keybinds assigned.
         private void Update()
         {
 
-            if (!MonoSingleton<OptionsManager>.Instance.inIntro && !MonoSingleton<OptionsManager>.Instance.paused && !MonoSingleton<NewMovement>.Instance.dead)
+            //// Plugin Config Init \\\\
 
+            if (configCreated == false && MonoSingleton<ColorBlindSettings>.Instance != null)
             {
 
-                /// Revovler \\\
+                PluginConfig.InitConfig();
 
-                #region
-
-                // Piercer (BLUE)
-                if (UnityInput.Current.GetKeyDown((KeyCode)RevovlerBlue.value))
-                {
-
-                    EquipWeapon(Guns.RevovlerBlue);
-
-                }
-
-
-
-                // Marksman (GREEN)
-                if (UnityInput.Current.GetKeyDown((KeyCode)RevovlerGreen.value))
-                {
-
-                    EquipWeapon(Guns.RevovlerGreen);
-
-                }
-
-
-
-                // Sharpshooter (RED)
-                if (UnityInput.Current.GetKeyDown((KeyCode)RevovlerRed.value))
-                {
-
-                    EquipWeapon(Guns.RevovlerRed);
-
-                }
-
-                #endregion
-
-
-                /// Shotgun \\\
-
-                #region
-
-                // Core Eject (BLUE)
-                if (UnityInput.Current.GetKeyDown((KeyCode)ShotgunBlue.value))
-                {
-
-                    EquipWeapon(Guns.ShotgunBlue);
-
-                }
-
-                // Pump (GREEN)
-                if (UnityInput.Current.GetKeyDown((KeyCode)ShotgunGreen.value))
-                {
-
-                    EquipWeapon(Guns.ShotgunGreen);
-
-                }
-
-                // Sawed On (RED)
-                if (UnityInput.Current.GetKeyDown((KeyCode)ShotgunRed.value))
-                {
-
-                    EquipWeapon(Guns.ShotgunRed);
-
-                }
-
-
-                #endregion
-
-
-                /// Nailgun \\\
-
-                #region
-
-                // Attractor (BLUE)
-                if (UnityInput.Current.GetKeyDown((KeyCode)NailgunBlue.value))
-                {
-
-                    EquipWeapon(Guns.NailgunBlue);
-
-                }
-
-                // Overheat (GREEN)
-                if (UnityInput.Current.GetKeyDown((KeyCode)NailgunGreen.value))
-                {
-
-                    EquipWeapon(Guns.NailgunGreen);
-
-                }
-
-                // Jumpstart (RED)
-                if (UnityInput.Current.GetKeyDown((KeyCode)NailgunRed.value))
-                {
-
-                    EquipWeapon(Guns.NailgunRed);
-
-                }
-
-                #endregion
-
-
-                /// Railcannon \\\
-
-                #region
-
-                // Electric (BLUE)
-                if (UnityInput.Current.GetKeyDown((KeyCode)RailcannonBlue.value))
-                {
-
-                    EquipWeapon(Guns.RailBlue);
-
-                }
-
-                // Screwdriver (GREEN)
-                if (UnityInput.Current.GetKeyDown((KeyCode)RailcannonGreen.value))
-                {
-
-                    EquipWeapon(Guns.RailGreen);
-
-                }
-
-                // Malicious (RED)
-                if (UnityInput.Current.GetKeyDown((KeyCode)RailcannonRed.value))
-                {
-
-                    EquipWeapon(Guns.RailRed);
-
-                }
-
-
-                #endregion
-
-
-                /// Rocket Launcher \\\
-
-                #region
-
-                // Freezeframe (BLUE)
-                if (UnityInput.Current.GetKeyDown((KeyCode)RocketBlue.value))
-                {
-
-                    EquipWeapon(Guns.RocketBlue);
-
-                }
-
-                // S.R.S (GREEN)
-                if (UnityInput.Current.GetKeyDown((KeyCode)RocketGreen.value))
-                {
-
-                    EquipWeapon(Guns.RocketGreen);
-
-                }
-
-                // Firestarter (RED)
-                if (UnityInput.Current.GetKeyDown((KeyCode)RocketRed.value))
-                {
-
-                    EquipWeapon(Guns.RocketRed);
-
-                }
-
-
-                #endregion
-
+                configCreated = true;
 
             }
 
+            //// Input \\\\
 
+            if (MonoSingleton<OptionsManager>.Instance != null && MonoSingleton<NewMovement>.Instance != null)
+            {
+
+                
+
+                if (!MonoSingleton<OptionsManager>.Instance.inIntro && !MonoSingleton<OptionsManager>.Instance.paused && !MonoSingleton<NewMovement>.Instance.dead)
+
+                {
+
+                    /// Revovler \\\
+
+                    #region
+
+                    // Piercer (BLUE)
+                    if (UnityInput.Current.GetKeyDown(PluginConfig.RevovlerBlue.value))
+                    {
+
+                        EquipWeapon(Guns.RevovlerBlue);
+
+                    }
+
+
+
+                    // Marksman (GREEN)
+                    if (UnityInput.Current.GetKeyDown(PluginConfig.RevovlerGreen.value))
+                    {
+
+                        EquipWeapon(Guns.RevovlerGreen);
+
+                    }
+
+
+
+                    // Sharpshooter (RED)
+                    if (UnityInput.Current.GetKeyDown(PluginConfig.RevovlerRed.value))
+                    {
+
+                        EquipWeapon(Guns.RevovlerRed);
+
+                    }
+
+                    #endregion
+
+
+                    /// Shotgun \\\
+
+                    #region
+
+                    // Core Eject (BLUE)
+                    if (UnityInput.Current.GetKeyDown(PluginConfig.ShotgunBlue.value))
+                    {
+
+                        EquipWeapon(Guns.ShotgunBlue);
+
+                    }
+
+                    // Pump (GREEN)
+                    if (UnityInput.Current.GetKeyDown(PluginConfig.ShotgunGreen.value))
+                    {
+
+                        EquipWeapon(Guns.ShotgunGreen);
+
+                    }
+
+                    // Sawed On (RED)
+                    if (UnityInput.Current.GetKeyDown(PluginConfig.ShotgunRed.value))
+                    {
+
+                        EquipWeapon(Guns.ShotgunRed);
+
+                    }
+
+
+                    #endregion
+
+
+                    /// Nailgun \\\
+
+                    #region
+
+                    // Attractor (BLUE)
+                    if (UnityInput.Current.GetKeyDown(PluginConfig.NailgunBlue.value))
+                    {
+
+                        EquipWeapon(Guns.NailgunBlue);
+
+                    }
+
+                    // Overheat (GREEN)
+                    if (UnityInput.Current.GetKeyDown(PluginConfig.NailgunGreen.value))
+                    {
+
+                        EquipWeapon(Guns.NailgunGreen);
+
+                    }
+
+                    // Jumpstart (RED)
+                    if (UnityInput.Current.GetKeyDown(PluginConfig.NailgunRed.value))
+                    {
+
+                        EquipWeapon(Guns.NailgunRed);
+
+                    }
+
+                    #endregion
+
+
+                    /// Railcannon \\\
+
+                    #region
+
+                    // Electric (BLUE)
+                    if (UnityInput.Current.GetKeyDown(PluginConfig.RailcannonBlue.value))
+                    {
+
+                        EquipWeapon(Guns.RailBlue);
+
+                    }
+
+                    // Screwdriver (GREEN)
+                    if (UnityInput.Current.GetKeyDown(PluginConfig.RailcannonGreen.value))
+                    {
+
+                        EquipWeapon(Guns.RailGreen);
+
+                    }
+
+                    // Malicious (RED)
+                    if (UnityInput.Current.GetKeyDown(PluginConfig.RailcannonRed.value))
+                    {
+
+                        EquipWeapon(Guns.RailRed);
+
+                    }
+
+
+                    #endregion
+
+
+                    /// Rocket Launcher \\\
+
+                    #region
+
+                    // Freezeframe (BLUE)
+                    if (UnityInput.Current.GetKeyDown(PluginConfig.RocketBlue.value))
+                    {
+
+                        EquipWeapon(Guns.RocketBlue);
+
+                    }
+
+                    // S.R.S (GREEN)
+                    if (UnityInput.Current.GetKeyDown(PluginConfig.RocketGreen.value))
+                    {
+
+                        EquipWeapon(Guns.RocketGreen);
+
+                    }
+
+                    // Firestarter (RED)
+                    if (UnityInput.Current.GetKeyDown(PluginConfig.RocketRed.value))
+                    {
+
+                        EquipWeapon(Guns.RocketRed);
+
+                    }
+
+
+                    #endregion
+
+
+                }
+
+
+
+            }
 
 
         }
---
+++ PluginConfig.cs
@@ -0,0 +1,155 @@
+﻿using GameConsole.pcon;
+using PluginConfig.API;
+using PluginConfig.API.Fields;
+using System.IO;
+using UnityEngine;
+
+
+namespace Easy_Swap
+{
+    public class PluginConfig
+    {
+
+        /// Keybinds \\\
+
+        #region
+
+        // Revovler
+
+        public static KeyCodeField RevovlerBlue;
+
+        public static KeyCodeField RevovlerGreen;
+
+        public static KeyCodeField RevovlerRed;
+
+
+        // Shotgun
+
+        public static KeyCodeField ShotgunBlue;
+
+        public static KeyCodeField ShotgunGreen;
+
+        public static KeyCodeField ShotgunRed;
+
+
+        // Nailgun
+
+        public static KeyCodeField NailgunBlue;
+
+        public static KeyCodeField NailgunGreen;
+
+        public static KeyCodeField NailgunRed;
+
+
+        // Railcannon
+
+        public static KeyCodeField RailcannonBlue;
+
+        public static KeyCodeField RailcannonGreen;
+
+        public static KeyCodeField RailcannonRed;
+
+
+        // Rocket Launcher
+
+        public static KeyCodeField RocketBlue;
+
+        public static KeyCodeField RocketGreen;
+
+        public static KeyCodeField RocketRed;
+
+        #endregion
+
+        public static void InitConfig()
+        {
+            var config = PluginConfigurator.Create("Easy Swap", Plugin.GUID);
+            config.SetIconWithURL($"{Path.Combine(Easy_Swap.Plugin.AssemblyFolder, "icon.png")}");
+
+            
+            /// Keybinds \\\
+
+            #region
+
+            Color[] variantColors = Plugin.GetVariantColors();
+
+
+            // Revovler \\
+
+            ConfigPanel RevovlerContainer = new ConfigPanel(config.rootPanel, "Revovler", "revlovPanelES");
+
+
+            RevovlerBlue = new KeyCodeField(RevovlerContainer, "Revovler (Piercer)", "RevlovES1", KeyCode.None);
+            RevovlerBlue.fieldColor = variantColors[0];
+
+            RevovlerGreen = new KeyCodeField(RevovlerContainer, "Revovler (Marksman)", "RevlovES2", KeyCode.None);
+            RevovlerGreen.fieldColor = variantColors[1];
+
+            RevovlerRed = new KeyCodeField(RevovlerContainer, "Revovler (Sharpshooter)", "RevlovES3", KeyCode.None);
+            RevovlerRed.fieldColor = variantColors[2];
+
+
+            // Shotgun \\
+
+            ConfigPanel ShotgunContainer = new ConfigPanel(config.rootPanel, "Shotgun", "ShotPanelES");
+
+
+            ShotgunBlue = new KeyCodeField(ShotgunContainer, "Shotgun (Core Eject)", "ShotES1", KeyCode.None);
+            ShotgunBlue.fieldColor = variantColors[0];
+
+            ShotgunGreen = new KeyCodeField(ShotgunContainer, "Shotgun (Pump)", "ShotES2", KeyCode.None);
+            ShotgunGreen.fieldColor = variantColors[1];
+
+            ShotgunRed = new KeyCodeField(ShotgunContainer, "Shotgun (Sawed On)", "ShotES3", KeyCode.None);
+            RevovlerRed.fieldColor = variantColors[2];
+
+
+            // Nailgun \\
+
+            ConfigPanel NailgunContainer = new ConfigPanel(config.rootPanel, "Nailgun", "NailPanelES");
+
+
+            NailgunBlue = new KeyCodeField(NailgunContainer, "Nailgun (Attractor)", "NailES1", KeyCode.None);
+            NailgunBlue.fieldColor = variantColors[0];
+
+            NailgunGreen = new KeyCodeField(NailgunContainer, "Nailgun (Overheat)", "NailES2", KeyCode.None);
+            NailgunGreen.fieldColor = variantColors[1];
+
+            NailgunRed = new KeyCodeField(NailgunContainer, "Nailgun (Jumpstart)", "NailES3", KeyCode.None);
+            NailgunRed.fieldColor = variantColors[2];
+
+
+            // Railcannon \\
+
+            ConfigPanel RailcannonContainer = new ConfigPanel(config.rootPanel, "Railcannon", "railPanelES");
+
+
+            RailcannonBlue = new KeyCodeField(RailcannonContainer, "Railcannon (Electric)", "RailES1", KeyCode.None);
+            RailcannonBlue.fieldColor = variantColors[0];
+
+            RailcannonGreen = new KeyCodeField(RailcannonContainer, "Railcannon (Screwdriver)", "RailES2", KeyCode.None);
+            RailcannonGreen.fieldColor = variantColors[1];
+
+            RailcannonRed = new KeyCodeField(RailcannonContainer, "Railcannon (Malicious)", "RailES3", KeyCode.None);
+            RailcannonRed.fieldColor = variantColors[2];
+
+
+            // Rocket Launcher \\
+
+            ConfigPanel RocketContainer = new ConfigPanel(config.rootPanel, "Rocket Launcher", "railPanelES");
+
+
+            RocketBlue = new KeyCodeField(RocketContainer, "Rocket (Freeze Frame)", "RocketES1", KeyCode.None);
+            RocketBlue.fieldColor = variantColors[0];
+
+            RocketGreen = new KeyCodeField(RocketContainer, "Rocket (S.R.S)", "RocketlES2", KeyCode.None);
+            RocketGreen.fieldColor = variantColors[1];
+
+            RocketRed = new KeyCodeField(RocketContainer, "Rocket (Firestarter)", "RocketES3", KeyCode.None);
+            RocketRed.fieldColor = variantColors[2];
+
+            #endregion
+
+        }
+
+    }
+}
